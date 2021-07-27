bf32a4068854487cb011ab9d3f0ffa6c492025e5
daeun-react <smile_daeun@naver.com>
2021-07-27 23:29:44 +0900

    refactor: key값 중복 방지를 위해 uuid 적용

--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,7 @@ import React from "react";
 import Item from "./Item";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
 
 function List() {
   //* comments 값 불러오기
@@ -9,8 +10,8 @@ function List() {
 
   return (
     <Container>
-      {comments.map((comment, index) => {
-        return <Item key={`comment${index}`} {...comment} />;
+      {comments.map((comment) => {
+        return <Item key={uuidv4()} {...comment} />;
       })}
     </Container>
   );
